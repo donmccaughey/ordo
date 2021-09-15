7b14e66d2de90369b06d98a3ca93c435fb4440c9
Don McCaughey <don@donm.cc>
2021-09-14 21:23:00 -0700

    Renamed `RomanNumeral` struct to `Numerus`.
    Changed `Numerus` from a tuple struct to a struct with one private member, `vis` ("strength, value") that holds and hides the underlying `u16`.
    
    Implemented the `Default` trait for `Numerus` with a default value of 1.  Updated the `Display` trait, making values 0 and >4000 invalid and adding display values "I" through "X".
    
    Implemented the `From` trait for `Numerus` to `u16` conversions and implemented the `TryFrom` trait for `u16` to `Numerus` conversions, failing for 0 and >4000.  Created error struct `Nimis` ("too much") as the `TryFrom` error type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,36 @@
-use std::fmt;
+use std::{fmt, error};
+use std::convert::TryFrom;
 use std::fmt::Formatter;
 
 
-struct RomanNumeral(u16);
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Numerus {
+    vis: u16,
+}
 
 
-impl fmt::Display for RomanNumeral {
+impl Default for Numerus {
+    fn default() -> Self {
+        Numerus { vis: 1 }
+    }
+}
+
+
+impl fmt::Display for Numerus {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let s = match self.0 {
-            0 => "nihil",
+        let s = match self.vis {
+            0 => panic!(),
             1 => "I",
             2 => "II",
+            3 => "III",
+            4 => "IV",
+            5 => "V",
+            6 => "VI",
+            7 => "VII",
+            8 => "VIII",
+            9 => "IX",
+            10 => "X",
+            4000.. => panic!(),
             _ => "?",
         };
         f.write_str(s)
@@ -18,19 +38,92 @@ impl fmt::Display for RomanNumeral {
 }
 
 
+impl From<Numerus> for u16 {
+    fn from(n: Numerus) -> Self {
+        n.vis
+    }
+}
+
+
+#[derive(Debug)]
+struct Nimis;
+
+
+impl fmt::Display for Nimis {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "nimis")
+    }
+}
+
+
+impl error::Error for Nimis {}
+
+
+impl TryFrom<u16> for Numerus {
+    type Error = Nimis;
+
+    fn try_from(vis: u16) -> Result<Self, Self::Error> {
+        if vis < 1 || vis > 3999 {
+            Err(Nimis)
+        } else {
+            Ok(Numerus { vis })
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
-    use crate::RomanNumeral;
+    use crate::Numerus;
+    use std::convert::TryFrom;
 
     #[test]
-    fn to_strings() {
-        let actual_expected = [
-            (0, "nihil"),
+    fn test_default() {
+        let n = Numerus::default();
+        assert_eq!("I", &n.to_string());
+    }
+
+    #[test]
+    fn test_to_strings() {
+        let verus_expectans = [
             (1, "I"),
             (2, "II"),
+            (3, "III"),
+            (4, "IV"),
+            (5, "V"),
+            (6, "VI"),
+            (7, "VII"),
+            (8, "VIII"),
+            (9, "IX"),
+            (10, "X"),
         ];
-        for (actual, expected) in actual_expected {
-            assert_eq!(expected, &RomanNumeral(actual).to_string());
+        for (verus, expectans) in verus_expectans {
+            let n = Numerus::try_from(verus).unwrap();
+            assert_eq!(expectans, &n.to_string());
         }
+    }
+
+    #[test]
+    fn test_into_u16() {
+        let n = Numerus::try_from(42).unwrap();
+        let u: u16 = n.into();
+        assert_eq!(42, u);
+    }
+
+    #[test]
+    fn test_try_from() {
+        let n0 = Numerus::try_from(0);
+        assert!(n0.is_err());
+
+        let n1 = Numerus::try_from(1);
+        assert!(n1.is_ok());
+        assert_eq!(1, n1.unwrap().vis);
+
+        let n3999 = Numerus::try_from(3999);
+        assert!(n3999.is_ok());
+        assert_eq!(3999, n3999.unwrap().vis);
+
+        let n4000 = Numerus::try_from(4000);
+        assert!(n4000.is_err());
     }
 }
