fa5dec18231fe36e05abdc19052c46817751e8ef
Don McCaughey <don@donm.cc>
2021-09-26 12:49:48 -0700

    Add `TryFrom` impls for more integer types.
    Add `TryFrom<T> for Numerus` impls for `i8`, `i16`, `i32`, `u8` and
    `u32`.
    
    Add tests for each new impl, plus a test of `try_from()` using
    unqualified integer literals.

--- src/numerus/try_from.rs
+++ src/numerus/try_from.rs
@@ -1,21 +1,21 @@
 use crate::errors::Nimis;
 use crate::numerus::Numerus;
 
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
 
-impl TryFrom<u16> for Numerus {
+impl TryFrom<i8> for Numerus {
     type Error = Nimis;
 
-    /// Create a _Numerus_ from an integer.
+    /// Create a _Numerus_ from an i8 integer.
     /// ```
     /// use ordo::Numerus;
     /// use std::convert::TryFrom;
     /// use std::convert::TryInto;
     ///
-    /// let xxvi = Numerus::try_from(26).unwrap();
+    /// let xxvi = Numerus::try_from(26_i8).unwrap();
     /// assert_eq!("XXVI", &xxvi.to_string());
     ///
-    /// let cxi: Numerus = 111.try_into().unwrap();
+    /// let cxi: Numerus = 111_i8.try_into().unwrap();
     /// assert_eq!("CXI", &cxi.to_string());
     /// ```
     ///
@@ -24,15 +24,200 @@ impl TryFrom<u16> for Numerus {
     /// use ordo::Numerus;
     /// use std::convert::TryFrom;
     ///
-    /// let result = Numerus::try_from(0);
+    /// let result = Numerus::try_from(-128_i8);
     /// assert!(matches!(result, Err(Nimis)));
     ///
-    /// let result = Numerus::try_from(4000);
+    /// let result = Numerus::try_from(0_i8);
+    /// assert!(matches!(result, Err(Nimis)));
+    /// ```
+    fn try_from(vis: i8) -> Result<Self, Self::Error> {
+        if vis < 1 {
+            Err(Nimis)
+        } else {
+            Ok(Numerus {
+                vis: vis.try_into().unwrap(),
+            })
+        }
+    }
+}
+
+impl TryFrom<i16> for Numerus {
+    type Error = Nimis;
+
+    /// Create a _Numerus_ from an i16 integer.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    /// use std::convert::TryInto;
+    ///
+    /// let xxvi = Numerus::try_from(26_i16).unwrap();
+    /// assert_eq!("XXVI", &xxvi.to_string());
+    ///
+    /// let cxi: Numerus = 111_i16.try_into().unwrap();
+    /// assert_eq!("CXI", &cxi.to_string());
+    /// ```
+    ///
+    /// If the integer is outside the range `1..=3999`, `Err(Nimis)` (_too much_) is returned.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    ///
+    /// let result = Numerus::try_from(0_i16);
+    /// assert!(matches!(result, Err(Nimis)));
+    ///
+    /// let result = Numerus::try_from(4000_i16);
+    /// assert!(matches!(result, Err(Nimis)));
+    /// ```
+    fn try_from(vis: i16) -> Result<Self, Self::Error> {
+        if (1..=3999).contains(&vis) {
+            Ok(Numerus {
+                vis: vis.try_into().unwrap(),
+            })
+        } else {
+            Err(Nimis)
+        }
+    }
+}
+
+impl TryFrom<i32> for Numerus {
+    type Error = Nimis;
+
+    /// Create a _Numerus_ from an i32 integer.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    /// use std::convert::TryInto;
+    ///
+    /// let xxvi = Numerus::try_from(26_i32).unwrap();
+    /// assert_eq!("XXVI", &xxvi.to_string());
+    ///
+    /// let cxi: Numerus = 111_i32.try_into().unwrap();
+    /// assert_eq!("CXI", &cxi.to_string());
+    /// ```
+    ///
+    /// If the integer is outside the range `1..=3999`, `Err(Nimis)` (_too much_) is returned.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    ///
+    /// let result = Numerus::try_from(0_i32);
+    /// assert!(matches!(result, Err(Nimis)));
+    ///
+    /// let result = Numerus::try_from(4000_i32);
+    /// assert!(matches!(result, Err(Nimis)));
+    /// ```
+    fn try_from(vis: i32) -> Result<Self, Self::Error> {
+        if (1..=3999).contains(&vis) {
+            Ok(Numerus {
+                vis: vis.try_into().unwrap(),
+            })
+        } else {
+            Err(Nimis)
+        }
+    }
+}
+
+impl TryFrom<u8> for Numerus {
+    type Error = Nimis;
+
+    /// Create a _Numerus_ from a u8 integer.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    /// use std::convert::TryInto;
+    ///
+    /// let xxvi = Numerus::try_from(26_u8).unwrap();
+    /// assert_eq!("XXVI", &xxvi.to_string());
+    ///
+    /// let cxi: Numerus = 111_u8.try_into().unwrap();
+    /// assert_eq!("CXI", &cxi.to_string());
+    /// ```
+    ///
+    /// If the integer is outside the range `1..=3999`, `Err(Nimis)` (_too much_) is returned.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    ///
+    /// let result = Numerus::try_from(0_u8);
+    /// assert!(matches!(result, Err(Nimis)));
+    /// ```
+    fn try_from(vis: u8) -> Result<Self, Self::Error> {
+        if vis < 1 {
+            Err(Nimis)
+        } else {
+            Ok(Numerus { vis: vis.into() })
+        }
+    }
+}
+
+impl TryFrom<u16> for Numerus {
+    type Error = Nimis;
+
+    /// Create a _Numerus_ from a u16 integer.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    /// use std::convert::TryInto;
+    ///
+    /// let xxvi = Numerus::try_from(26_u16).unwrap();
+    /// assert_eq!("XXVI", &xxvi.to_string());
+    ///
+    /// let cxi: Numerus = 111_u16.try_into().unwrap();
+    /// assert_eq!("CXI", &cxi.to_string());
+    /// ```
+    ///
+    /// If the integer is outside the range `1..=3999`, `Err(Nimis)` (_too much_) is returned.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    ///
+    /// let result = Numerus::try_from(0_u16);
+    /// assert!(matches!(result, Err(Nimis)));
+    ///
+    /// let result = Numerus::try_from(4000_u16);
     /// assert!(matches!(result, Err(Nimis)));
     /// ```
     fn try_from(vis: u16) -> Result<Self, Self::Error> {
         if (1..=3999).contains(&vis) {
             Ok(Numerus { vis })
+        } else {
+            Err(Nimis)
+        }
+    }
+}
+
+impl TryFrom<u32> for Numerus {
+    type Error = Nimis;
+
+    /// Create a _Numerus_ from a u32 integer.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    /// use std::convert::TryInto;
+    ///
+    /// let xxvi = Numerus::try_from(26_u32).unwrap();
+    /// assert_eq!("XXVI", &xxvi.to_string());
+    ///
+    /// let cxi: Numerus = 111_u32.try_into().unwrap();
+    /// assert_eq!("CXI", &cxi.to_string());
+    /// ```
+    ///
+    /// If the integer is outside the range `1..=3999`, `Err(Nimis)` (_too much_) is returned.
+    /// ```
+    /// use ordo::Numerus;
+    /// use std::convert::TryFrom;
+    ///
+    /// let result = Numerus::try_from(0_u32);
+    /// assert!(matches!(result, Err(Nimis)));
+    ///
+    /// let result = Numerus::try_from(4000_u32);
+    /// assert!(matches!(result, Err(Nimis)));
+    /// ```
+    fn try_from(vis: u32) -> Result<Self, Self::Error> {
+        if (1..=3999).contains(&vis) {
+            Ok(Numerus {
+                vis: vis.try_into().unwrap(),
+            })
         } else {
             Err(Nimis)
         }
--- src/numerus/try_from_tests.rs
+++ src/numerus/try_from_tests.rs
@@ -1,9 +1,9 @@
 use crate::errors::Nimis;
 use crate::numerus::Numerus;
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
 
 #[test]
-fn test_try_from() {
+fn test_numerus_try_from_unqualified_integer_literal() {
     let result = Numerus::try_from(0);
     assert!(matches!(result, Err(Nimis)));
 
@@ -20,25 +20,121 @@ fn test_try_from() {
 }
 
 #[test]
-fn test_try_from_i8() {
-    let n = Numerus::try_from(127).unwrap();
+fn test_numerus_try_from_i8() {
+    let result = Numerus::try_from(0_i8);
+    assert!(matches!(result, Err(Nimis)));
+
+    let result = Numerus::try_from(1_i8);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().vis);
+
+    let result = Numerus::try_from(i8::MAX);
+    assert!(result.is_ok());
+    assert_eq!(127, result.unwrap().vis);
+}
+
+#[test]
+fn test_numerus_try_from_i16() {
+    let result = Numerus::try_from(0_i16);
+    assert!(matches!(result, Err(Nimis)));
+
+    let result = Numerus::try_from(1_i16);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().vis);
+
+    let result = Numerus::try_from(3999_i16);
+    assert!(result.is_ok());
+    assert_eq!(3999, result.unwrap().vis);
+
+    let result = Numerus::try_from(4000_i16);
+    assert!(matches!(result, Err(Nimis)));
+}
+
+#[test]
+fn test_numerus_try_from_i32() {
+    let result = Numerus::try_from(0_i32);
+    assert!(matches!(result, Err(Nimis)));
+
+    let result = Numerus::try_from(1_i32);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().vis);
+
+    let result = Numerus::try_from(3999_i32);
+    assert!(result.is_ok());
+    assert_eq!(3999, result.unwrap().vis);
+
+    let result = Numerus::try_from(4000_i32);
+    assert!(matches!(result, Err(Nimis)));
+}
+
+#[test]
+fn test_numerus_try_from_u8() {
+    let result = Numerus::try_from(0_u8);
+    assert!(matches!(result, Err(Nimis)));
+
+    let result = Numerus::try_from(1_u8);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().vis);
+
+    let result = Numerus::try_from(u8::MAX);
+    assert!(result.is_ok());
+    assert_eq!(255, result.unwrap().vis);
+}
+
+#[test]
+fn test_numerus_try_from_u16() {
+    let result = Numerus::try_from(0_u16);
+    assert!(matches!(result, Err(Nimis)));
+
+    let result = Numerus::try_from(1_u16);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().vis);
+
+    let result = Numerus::try_from(3999_u16);
+    assert!(result.is_ok());
+    assert_eq!(3999, result.unwrap().vis);
+
+    let result = Numerus::try_from(4000_u16);
+    assert!(matches!(result, Err(Nimis)));
+}
+
+#[test]
+fn test_numerus_try_from_u32() {
+    let result = Numerus::try_from(0_u32);
+    assert!(matches!(result, Err(Nimis)));
+
+    let result = Numerus::try_from(1_u32);
+    assert!(result.is_ok());
+    assert_eq!(1, result.unwrap().vis);
+
+    let result = Numerus::try_from(3999_u32);
+    assert!(result.is_ok());
+    assert_eq!(3999, result.unwrap().vis);
+
+    let result = Numerus::try_from(4000_u32);
+    assert!(matches!(result, Err(Nimis)));
+}
+
+#[test]
+fn test_i8_try_from_numerus() {
+    let n: Numerus = 127.try_into().unwrap();
     let result = i8::try_from(n);
     assert!(result.is_ok());
     assert_eq!(i8::MAX, result.unwrap());
 
-    let n = Numerus::try_from(128).unwrap();
+    let n: Numerus = 128.try_into().unwrap();
     let result = i8::try_from(n);
     assert!(matches!(result, Err(Nimis)));
 }
 
 #[test]
-fn test_try_from_u8() {
-    let n = Numerus::try_from(255).unwrap();
+fn test_u8_try_from_numerus() {
+    let n: Numerus = 255.try_into().unwrap();
     let result = u8::try_from(n);
     assert!(result.is_ok());
     assert_eq!(u8::MAX, result.unwrap());
 
-    let n = Numerus::try_from(256).unwrap();
+    let n: Numerus = 256.try_into().unwrap();
     let result = u8::try_from(n);
     assert!(matches!(result, Err(Nimis)));
 }
