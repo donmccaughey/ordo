5ad21e1f3403377fb92040d9d64fe446cd6bd0d2
Don McCaughey <don@donm.cc>
2021-09-25 20:45:19 -0700

    Added direct test for `From<Numerus> for u16`.
    This complements the existing `Into<u16> for Numerus` test.

--- src/numerus/from_tests.rs
+++ src/numerus/from_tests.rs
@@ -3,6 +3,13 @@ use std::convert::TryFrom;
 use crate::numerus::Numerus;
 
 #[test]
+fn test_u16_from() {
+    let xlii = Numerus::try_from(42).unwrap();
+    let u = u16::from(xlii);
+    assert_eq!(42, u);
+}
+
+#[test]
 fn test_into_u16() {
     let xlii = Numerus::try_from(42).unwrap();
     let u: u16 = xlii.into();
