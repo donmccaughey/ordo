0b87d356bfa1f6ec6f885d73e371b26d74c64917
Don McCaughey <don@donm.cc>
2021-09-27 22:00:01 -0700

    Added doc comments and doc tests for `From` impls.
    Add example uses of `from()` and `into()` on the `From<Numerus>` impls
    for `i16`, `i32`, `i64`, `i128`, `isize`, `u16`, `u32`, `u64`, `u128`
    and `usize`.

--- src/numerus/from.rs
+++ src/numerus/from.rs
@@ -2,6 +2,18 @@ use crate::Numerus;
 
 use std::convert::TryInto;
 
+/// Convert a _Numerus_ into an [i16] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = i16::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: i16 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for i16 {
     fn from(n: Numerus) -> Self {
         debug_assert!(Numerus::MAX.vis < i16::MAX as u16);
@@ -9,24 +21,72 @@ impl From<Numerus> for i16 {
     }
 }
 
+/// Convert a _Numerus_ into an [i32] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = i32::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: i32 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for i32 {
     fn from(n: Numerus) -> Self {
         n.vis.into()
     }
 }
 
+/// Convert a _Numerus_ into an [i64] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = i64::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: i64 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for i64 {
     fn from(n: Numerus) -> Self {
         n.vis.into()
     }
 }
 
+/// Convert a _Numerus_ into an [i64] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = i128::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: i128 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for i128 {
     fn from(n: Numerus) -> Self {
         n.vis.into()
     }
 }
 
+/// Convert a _Numerus_ into an [isize] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = isize::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: isize = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for isize {
     fn from(n: Numerus) -> Self {
         debug_assert!((Numerus::MAX.vis as usize) < (isize::MAX as usize));
@@ -34,30 +94,90 @@ impl From<Numerus> for isize {
     }
 }
 
+/// Convert a _Numerus_ into a [u16] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = u16::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: u16 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for u16 {
     fn from(n: Numerus) -> Self {
         n.vis
     }
 }
 
+/// Convert a _Numerus_ into a [u32] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = u32::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: u32 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for u32 {
     fn from(n: Numerus) -> Self {
         n.vis.into()
     }
 }
 
+/// Convert a _Numerus_ into a [u64] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = u64::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: u64 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for u64 {
     fn from(n: Numerus) -> Self {
         n.vis.into()
     }
 }
 
+/// Convert a _Numerus_ into a [u128] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = u128::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: u128 = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for u128 {
     fn from(n: Numerus) -> Self {
         n.vis.into()
     }
 }
 
+/// Convert a _Numerus_ into a [usize] integer.
+/// ```
+/// use ordo::Numerus;
+/// use std::str::FromStr;
+/// let xlii = Numerus::from_str("XLII").unwrap();
+///
+/// let a = usize::from(xlii);
+/// assert_eq!(a, 42);
+///
+/// let b: usize = xlii.into();
+/// assert_eq!(b, 42);
+/// ```
 impl From<Numerus> for usize {
     fn from(n: Numerus) -> Self {
         n.vis.into()
