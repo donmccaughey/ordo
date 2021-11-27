9677c631658855a71c7d5689f97397956f5c3d52
Don McCaughey <don@donm.cc>
2021-11-27 02:38:42 -0800

    Rework initialization methods for `Locutio`.
    Renamed `from_word()` to `one_word()`.  Added initializer `two_words()`.
    These are both infallible constructors.
    
    Changed `new()` to `try_new()`, which now returns a `Result<Locutio,
    Irritus>`.  `try_new()` fails if the provided vector is empty.

--- src/locutio/mod.rs
+++ src/locutio/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod mod_tests;
 
+use crate::errors::Irritus;
 use crate::Orthographia;
 
 pub struct Locutio {
@@ -8,13 +9,23 @@ pub struct Locutio {
 }
 
 impl Locutio {
-    pub fn from_word(orthographia: Orthographia) -> Locutio {
+    pub fn one_word(orthographia: Orthographia) -> Locutio {
         Locutio {
             orthographiae: vec![orthographia],
         }
     }
 
-    pub fn new(orthographiae: Vec<Orthographia>) -> Locutio {
-        Locutio { orthographiae }
+    pub fn two_words(orthographia1: Orthographia, orthographia2: Orthographia) -> Locutio {
+        Locutio {
+            orthographiae: vec![orthographia1, orthographia2],
+        }
+    }
+
+    pub fn try_new(orthographiae: Vec<Orthographia>) -> Result<Locutio, Irritus> {
+        if orthographiae.len() == 0 {
+            Err(Irritus)
+        } else {
+            Ok(Locutio { orthographiae })
+        }
     }
 }
--- src/locutio/mod_tests.rs
+++ src/locutio/mod_tests.rs
@@ -2,19 +2,37 @@ use crate::locutio::Locutio;
 use crate::Orthographia;
 
 #[test]
-fn test_from_word() {
+fn test_one_word() {
     let canis = Orthographia::try_from_ascii("canis").unwrap();
-    let locutio = Locutio::from_word(canis);
+    let locutio = Locutio::one_word(canis);
     assert_eq!(1, locutio.orthographiae.len());
     assert_eq!("canis", &locutio.orthographiae[0].to_string());
 }
 
 #[test]
-fn test_new() {
+fn test_two_words() {
     let amatus = Orthographia::try_from_ascii("amatus").unwrap();
     let es = Orthographia::try_from_ascii("es").unwrap();
-    let locutio = Locutio::new(vec![amatus, es]);
+    let locutio = Locutio::two_words(amatus, es);
     assert_eq!(2, locutio.orthographiae.len());
     assert_eq!("amatus", &locutio.orthographiae[0].to_string());
     assert_eq!("es", &locutio.orthographiae[1].to_string());
+}
+
+#[test]
+fn test_try_new() {
+    let amatus = Orthographia::try_from_ascii("amatus").unwrap();
+    let es = Orthographia::try_from_ascii("es").unwrap();
+    let result = Locutio::try_new(vec![amatus, es]);
+    assert!(result.is_ok());
+    let locutio = result.unwrap();
+    assert_eq!(2, locutio.orthographiae.len());
+    assert_eq!("amatus", &locutio.orthographiae[0].to_string());
+    assert_eq!("es", &locutio.orthographiae[1].to_string());
+}
+
+#[test]
+fn test_try_new_irritus() {
+    let result = Locutio::try_new(vec![]);
+    assert!(matches!(result, Err(Irritus)));
 }
